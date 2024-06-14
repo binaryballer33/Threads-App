cd123a49efbe913600329636ea8a3537ae1d310d
Shaquille Mandy <shaqmandy@gmail.com>
2024-06-14 21:14:17 +0700

    changed userInfo object

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,29 +1,36 @@
-import AccountProfile from '@/components/forms/AccountProfile'
-import { currentUser } from '@clerk/nextjs/server'
-import React from 'react'
+import AccountProfile from "@/components/forms/AccountProfile";
+import { getUser } from "@/lib/actions/users/getUser.action";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
 async function Page() {
-	const user = await currentUser()
+  const user = await currentUser();
 
-	const userData = {
-		id: user?.id as string,
-		username: user?.username as string,
-		name: user?.firstName as string,
-		image: user?.imageUrl as string,
-	}
+  if (!user) return null;
 
-	return (
-		<main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-			<h1 className="head-text">Onboarding</h1>
-			<p className="text-base-regular text-light-2 mt-3">
-				Complete your onboarding now to use Threads
-			</p>
+  const userInfo = await getUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-			<section className="mt-9 p-10 bg-dark-2">
-				<AccountProfile user={userData} />
-			</section>
-		</main>
-	)
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user?.username,
+    name: userInfo ? userInfo?.name : user?.firstName || "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user?.imageUrl,
+  };
+
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="text-base-regular text-light-2 mt-3">Complete your onboarding now to use Threads</p>
+
+      <section className="mt-9 p-10 bg-dark-2">
+        <AccountProfile user={userData} />
+      </section>
+    </main>
+  );
 }
 
-export default Page
+export default Page;
--- src/app/(root)/communities/page.tsx
+++ src/app/(root)/communities/page.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import UserCard from "@/components/cards/UserCard";
+import { getUser } from "@/lib/actions/users/getUser.action";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getCommunities } from "@/lib/actions/communities/getCommunities.action";
+import CommunityCard from "@/components/cards/CommunityCard";
 
 async function CommunitiesPage() {
+  const user = await currentUser();
+
+  if (!user) return null;
+  const userInfo = await getUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const { communities, isNext } = await getCommunities({});
+
   return (
-    <section>
+    <section className="mx-10">
       <h1 className="head-text mb-10">Communities Page</h1>
+      <div className="mt-14 gap-9 flex flex-col">
+        {communities.length === 0 ? (
+          <p className="no-result">No Users</p>
+        ) : (
+          <>
+            {communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
