dbf25ca93bfbdbb9d988abf903a7c575ed921098
Shaquille Rashad Mandy <shaqmandy@gmail.com>
2024-11-16 18:32:30 -0600

    added any to fix build

--- src/lib/actions/threads/getAllChildThreads.action.ts
+++ src/lib/actions/threads/getAllChildThreads.action.ts
@@ -3,7 +3,7 @@ import { Thread } from "@/lib/models/thread.model";
 export async function getAllChildThreads(threadId: string): Promise<any[]> {
   const childThreads = await Thread.find({ parentId: threadId });
 
-  const descendantThreads = [];
+  const descendantThreads: any[] = [];
   for (const childThread of childThreads) {
     const descendants = await getAllChildThreads(childThread._id);
     descendantThreads.push(childThread, ...descendants);
